ee1fed15c5cd0d0fa53a31448a7b12544d76c701
lexaeliseev <lexaeliseev@gmail.com>
2024-10-16 23:00:21 +0300

    reformat code

--- test_archive_practice.py
+++ test_archive_practice.py
@@ -7,9 +7,7 @@ from openpyxl import load_workbook
 
 def test_check_pdf(create_zip_file):
     with ZipFile(f"{TMP_DIR}/archive.zip") as zip_file:
-        print("---")
         print(f"Список файлов в архиве: {zip_file.namelist()}")
-        print("---")
         with zip_file.open("pdf_file.pdf") as pdf_file:
             reader_pdf = PdfReader(pdf_file)
             count_pages = len(reader_pdf.pages)
@@ -26,11 +24,9 @@ def test_check_xlsx(create_zip_file):
         with zip_file.open("import_ou.xlsx") as xlsx_file:
             workbook = load_workbook(xlsx_file)
 
-            print("---")
             count_pages = len(workbook.sheetnames)
             print(f"Количество листов в файле XLSX: {count_pages}")
             assert count_pages == 1
-            print("---")
 
             sheet = workbook.active
             print(f"Название активного листа в файле XLSX: {sheet.title}")
@@ -47,19 +43,16 @@ def test_check_csv(create_zip_file):
             content = csv_file.read().decode('utf-8-sig')
             reader_csv = list(csv.reader(content.splitlines()))
 
-            print("---")
             first_row = reader_csv[0]
             count_columns = len(first_row)
             print(f"Количество колонок в файле: {count_columns}")
             assert count_columns == 3
 
-            print("---")
             value = "Внешний идентификатор для импорта,Вышестоящий отдел,Название"
             list_value = value.split(",")
             print(f"Список заголовков в файле: {first_row}")
             assert first_row == list_value
 
-            print("---")
             count_row = sum(1 for _ in reader_csv)
             print(f"Количество строк в файле: {count_row}")
             assert count_row == 7
